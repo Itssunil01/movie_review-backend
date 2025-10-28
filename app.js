019b77e73eef1b502eb9f75ea03ee6bc4939934f
Itssunil01 <balsunilkumar34@gmail.com>
2025-10-29 00:19:39 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ require("dotenv").config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(cors({
+ origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+}));
 
 const port = process.env.PORT || 3000;
 const mongoUrl = process.env.MONGOURL;
