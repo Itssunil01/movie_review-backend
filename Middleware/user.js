36c83d407deda765041c61409f4f94679373bb23
Itssunil01 <balsunilkumar34@gmail.com>
2025-10-28 17:20:22 +0530

    done some changes

--- Middleware/user.js
+++ Middleware/user.js
@@ -3,7 +3,7 @@ const signupSchema = require("../SchemaValidation/user");
 module.exports.signupValidation = (req,res,next) => {
     const { err } = signupSchema.validate(req.body);
     if(err){
-        return res.status(404).json({message: err , success: true});
+        return res.status(404).json({message: err , success: false});
     }
     next();
 }
