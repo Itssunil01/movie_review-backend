ac472a3491cc62102885b00d1d6a247156a39212
Itssunil01 <balsunilkumar34@gmail.com>
2025-10-29 00:14:40 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -1,52 +1,55 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const PORT = 3000;
-const mongoose = require("mongoose")
-const session = require("express-session")
+const mongoose = require("mongoose");
+const session = require("express-session");
 const userRouter = require("./Routes/user");
 const cors = require("cors");
 
-require("dotenv").config()
+require("dotenv").config();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors())
+app.use(cors());
 
 const port = process.env.PORT || 3000;
 const mongoUrl = process.env.MONGOURL;
 const secret = process.env.SECRET;
 
 const sessions = {
-    secret: secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000
-    }
+  secret: secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  },
 };
 
 app.use(session(sessions));
 
-
-main().then(() => {
-   console.log("connected")
-}).catch((err) => {
-  console.log(err)
-})
+main()
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 async function main() {
-  await mongoose.connect(mongoUrl);
+  await mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
 
-app.use("/" , userRouter);
+app.use("/", userRouter);
 
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
-app.get("/" , (req,res) => {
-    res.send("Hello")
-})
-
-app.listen(port , () => {
-    console.log("listening");
-})
+app.listen(port, () => {
+  console.log("listening");
+});
