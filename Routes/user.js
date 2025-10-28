9cc42e6a187224b4f91639a8cbc337a9d2db5b7b
Itssunil01 <balsunilkumar34@gmail.com>
2025-10-29 00:42:34 +0530

    changes in routes

--- Routes/user.js
+++ Routes/user.js
@@ -3,6 +3,6 @@ const { signup } = require("../Controller/user");
 const { signupValidation } = require("../Middleware/user");
 const router = express.Router();
 
-router.post("/signup", signupValidation , signup);
+router.post("/signup" , signup);
 
 module.exports = router;
